99088b15dce2aae184a7560181d4e1f59d1d28d2
Lex Bailey <gitcommit@lexbailey.me>
2024-04-07 10:00:12 +0100

    static init

--- crt0.s
+++ crt0.s
@@ -1,8 +1,14 @@
 	.module crt0
-	.globl	_main
+	.globl _main
 
-	.area	_HEADER (ABS)
-	.org 	0x8000
-	ld	sp,#0x0000
-	call	_main
+	.area _HEADER (ABS)
+	.org 0x8000
+	ld sp,#0x0000
+    call _do_gs_init
+	call _main
 	ret
+
+    .area _BEFORE_GS_INIT
+_do_gs_init:
+    .area _AFTER_GS_INIT
+    ret
